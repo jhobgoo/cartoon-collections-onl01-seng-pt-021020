bb915108fc692131c7a60b4cdf380a834c9dfc41
Joel Hobgood <jhobgoo@gmail.com>
2020-02-21 06:30:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,5 +20,5 @@ end
 
 def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  string.find { |string| cheese_types.include?(string)}
+  cheese.find { |string| cheese_types.include?(string)}
 end
