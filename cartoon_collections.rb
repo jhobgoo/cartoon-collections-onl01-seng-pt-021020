fcc892190922d9bafcde1532e50c3dfd12f3b7f5
Joel Hobgood <jhobgoo@gmail.com>
2020-02-21 06:00:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planateer_calls)
-  planateer_calls.map do |element|
+  planateer_calls.map do |planateer_calls|
     planateer_calls.capitalize + '!'
   end
 end
