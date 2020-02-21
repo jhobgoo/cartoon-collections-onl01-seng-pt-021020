b2eddb98cea0e391e2224c5f0c285ba93ce0e4c3
Joel Hobgood <jhobgoo@gmail.com>
2020-02-21 06:20:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,7 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
   array.find { |i| i == cheese_types }
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
